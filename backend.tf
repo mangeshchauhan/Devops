00084205b32bd985f95912bd03fb78d279147116
mangeshchauhan <mangesh.225@gmail.com>
2024-12-30 19:53:44 +0530

    adding new module and updating some files

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraformstatefile1234"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
