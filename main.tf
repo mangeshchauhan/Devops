8d813808aeee44efc7907b8699d4b52962756c8a
mangeshchauhan <mangesh.225@gmail.com>
2024-12-30 17:58:08 +0530

    updating Jenkins and main.tf

--- main.tf
+++ main.tf
@@ -26,4 +26,5 @@ module "ec2" {
   public_subnet_id    = module.vpc.public_subnet_id
   private_subnet_id   = module.vpc.private_subnet_id
   instance_type       = var.instance_type
+  key_name = ""
 }
