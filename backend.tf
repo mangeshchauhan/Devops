fcada3d85b823b78df22e4fc440488f40007eef3
mangeshchauhan <mangesh.225@gmail.com>
2024-12-30 17:41:31 +0530

    updating backend Configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "terraformstatefile1234"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
