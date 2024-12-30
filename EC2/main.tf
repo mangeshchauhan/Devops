d4149a4baa217fb89792ccc47b1050697212320e
mangeshchauhan <mangesh.225@gmail.com>
2024-12-30 13:02:53 +0530

    Initial Commit

---
+++ EC2/main.tf
@@ -0,0 +1,57 @@
+resource "aws_security_group" "ec2" {
+  name        = "ec2-security-group"
+  description = "Security group for EC2 instances"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
+
+resource "aws_instance" "public" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnet_id
+  key_name              = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  tags = {
+    Name = "public-instance"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = var.private_subnet_id
+  key_name              = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  tags = {
+    Name = "private-instance"
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
---
+++ EC2/variable.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "Public subnet ID"
+  type        = string
+}
+
+variable "private_subnet_id" {
+  description = "Private subnet ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
