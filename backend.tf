10d6883a12c6c883dcbc7cde740d87106fea5368
mangesh.225 <mangesh.225@gmail.com>
2025-07-11 17:15:31 +0530

    updating backend

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraformstatefile1234"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    use_lockfile   = true
-  }
-}
